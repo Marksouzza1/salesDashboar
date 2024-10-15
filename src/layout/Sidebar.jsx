d388bb43b8f166e78ae7bb5c5ecef4e62329950a
Marksouzza1 <marcogs713@gmail.com>
2024-10-15 18:04:53 -0300

    addes uselocation

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "../assets/css/Sidebar.module.css"
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     return (
         <>
         <div className={styles.sidebar}>
         <div className={styles.buttonContainer}>
-            <button onClick={() => navigate('/Dashboard')}>Dashboard</button>
-            <button onClick={() => navigate('/Customers')}>Clientes</button>
-            <button onClick={() => navigate('/produtos')}>Produtos</button>
+        <button
+                    className={location.pathname === '/dashboard' ? styles.active : ''}
+                    onClick={() => navigate('/dashboard')}
+                >Dashboard</button>
+            <button className={location.pathname === '/Customers' ? styles.active : ''}
+                 onClick={() => navigate('/Customers')}>Clientes</button>
+            <button className={location.pathname === '/produtos' ? styles.active : ''}
+                 onClick={() => navigate('/produtos')}>Produtos</button>
         </div>
 
         </div>
